5f26c19435cc9758a6e83542ea6d899bddc5a696
mrjeyson <71706561+mrjeyson@users.noreply.github.com>
2024-06-26 11:05:01 +0500

    ...

--- app/src/main/java/com/example/newsapp/presentaion/navgraph/NavGraph.kt
+++ app/src/main/java/com/example/newsapp/presentaion/navgraph/NavGraph.kt
@@ -14,6 +14,7 @@ import com.example.newsapp.presentaion.onboarding.OnBoardingViewModel
 fun NavGraph(
     startDestination: String
 ) {
+
     val navController = rememberNavController()
 
     NavHost(navController = navController, startDestination = startDestination) {
