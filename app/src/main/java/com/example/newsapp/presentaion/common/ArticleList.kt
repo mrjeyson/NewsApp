c1a931dd553fdead3d28ecb4a5061d835114abeb
mrjeyson <71706561+mrjeyson@users.noreply.github.com>
2024-07-31 11:19:49 +0500

    ...

--- app/src/main/java/com/example/newsapp/presentaion/common/ArticleList.kt
+++ app/src/main/java/com/example/newsapp/presentaion/common/ArticleList.kt
@@ -14,6 +14,7 @@ import com.example.newsapp.domain.model.Article
 import com.example.newsapp.presentaion.Dimens.ExtraSmallPadding2
 import com.example.newsapp.presentaion.Dimens.MediumPadding1
 
+
 @Composable
 fun ArticleList(
     modifier: Modifier = Modifier,
