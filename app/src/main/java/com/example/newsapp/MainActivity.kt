c9ea5ee563eee7bdfe36320d41377eeffe656c1e
mrjeyson <71706561+mrjeyson@users.noreply.github.com>
2024-10-15 08:14:06 +0500

    ...

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -38,6 +38,7 @@ class MainActivity : ComponentActivity() {
             ),
         )
 
+
         installSplashScreen().apply {
             setKeepOnScreenCondition {
                 viewModel.splashCondition
