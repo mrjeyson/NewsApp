88f2e43fe50665f327a2723137c788c891747649
mrjeyson <71706561+mrjeyson@users.noreply.github.com>
2025-03-05 08:51:05 +0500

    ....

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -49,7 +49,6 @@ class MainActivity : ComponentActivity() {
 
 
 
-
     private val viewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
