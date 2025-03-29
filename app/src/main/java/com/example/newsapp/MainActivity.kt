1c0c5c181b89ee47de40c7e7d26801d4a9becbf4
mrjeyson <71706561+mrjeyson@users.noreply.github.com>
2025-03-29 13:25:28 +0900

    ,...

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -27,6 +27,7 @@ import javax.inject.Inject
 class MainActivity : ComponentActivity() {
 
 
+
     private val viewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
