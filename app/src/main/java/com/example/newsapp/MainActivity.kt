5aab91069a09043a1b85a757704ebe1c3aa4df34
Jasurbek <jasurbekkurganbayev@gmail.com>
2025-05-12 10:17:51 +0900

    ...

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -46,7 +46,6 @@ class MainActivity : ComponentActivity(){
 
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
