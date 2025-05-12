543de2778d68bec9e5eb4da9c84ced1a4caa1378
Jasurbek <jasurbekkurganbayev@gmail.com>
2025-05-12 10:13:53 +0900

    ...

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -27,6 +27,8 @@ import javax.inject.Inject
 class MainActivity : ComponentActivity(){
     private val viewModel by viewModels<MainViewModel>()
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
