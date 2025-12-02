a54003278cf67e620d241aec6991ed07c6e32646
Jasurbek <jasurbekkurganbayev@gmail.com>
2025-12-02 10:05:31 +0900

    ...

--- app/src/main/java/com/example/newsapp/data/remote/dto/NewsResponse.kt
+++ app/src/main/java/com/example/newsapp/data/remote/dto/NewsResponse.kt
@@ -3,6 +3,7 @@ package com.example.newsapp.data.remote.dto
 import com.example.newsapp.domain.model.Article
 
 data class NewsResponse(
+    
     val articles: List<Article>,
     val status: String,
     val totalResults: Int
